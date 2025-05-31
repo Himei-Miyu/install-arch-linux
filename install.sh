96975b02b225291408f98d93f2f39b46f056a15d
Miyu <miyu@himei.city>
2025-06-01 01:12:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,18 @@ declare -A DATA
 logInfo() { echo -e "[\e[92mINFO\e[0m] \e[92m$@\e[0m"; }
 logWarn() { echo -e "[\e[93mWARN\e[0m] \e[93m$@\e[0m"; }
 logError() { echo -e "[\e[91mERROR\e[0m] \e[91m$@\e[0m"; }
-
+logLoop() {
+  while true; do
+    case "$1" in
+      [Yy]) break;;
+      [Nn]) exit 1;;
+      *) continue;;
+    esac
+  done
+}
+lsblk
 read -p "Location disk: " DATA[DISK]
-logWarn "This will erase $DISK !!!"
-read -p "`logInfo \"Do you want to continue? [y/N]: \"`"
-read -p "`logWarn \"Do you want to continue? [y/N]: \"`"
-read -p "`logError \"Do you want to continue? [y/N]: \"`"
+logWarn "This will erase ${DATA[DISK]} !!!"
+read -p "`logInfo \"Do you want to continue? [y/N]: \"`" DATA[isYes]
+logLoop ${DATA[isYes]}
+logInfo "processing..."
